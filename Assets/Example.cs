2e9510b8bc1a6be1e2bfe6d1f53742b9ff020842
denis <denispavlenko7610new@gmail.com>
2023-08-15 11:39:44 +0300

    init

---
+++ Assets/Example.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+using DefaultNamespace;
+
+public class RotationController : MonoBehaviour
+{
+    [SerializeField] Transform targetObject;
+
+    void Start()
+    {
+        RotateToObjectAsync().Forget();
+    }
+
+    async UniTask RotateToObjectAsync(float duration = 2f)
+    {
+        float startTime = Time.time;
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.Euler(0f, CalculateAngleCross(), 0f);
+
+        while (Time.time - startTime < duration)
+        {
+            float progress = (Time.time - startTime) / duration;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, progress);
+            await UniTask.Yield();
+        }
+
+        transform.rotation = targetRotation;
+    }
+
+    float CalculateAngleDot()
+    {
+        Vector3 targetDirection = targetObject.position - transform.position;
+        float yAngle = Mathf.Acos(MathFunc.Dot(Vector3.forward, targetDirection.normalized));
+        
+        print("yAngle: " + yAngle * Mathf.Rad2Deg);
+        return yAngle * Mathf.Rad2Deg;
+    }
+    
+    float CalculateAngleCross()
+    {
+        Vector3 toTarget = targetObject.position - transform.position;
+        Vector3 crossProduct = MathFunc.Cross(Vector3.forward, toTarget.normalized);
+        float yAngle = Mathf.Asin(crossProduct.magnitude);
+        
+        print("yAngle: " + yAngle * Mathf.Rad2Deg);
+        return yAngle * Mathf.Rad2Deg;
+    }
+}
---
+++ Assets/MathFunc.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class MathFunc
+    {
+        public static Vector3 Cross(Vector3 v, Vector3 w)
+        {
+            float xMult = v.y * w.z - v.z * w.y;
+            float yMult = v.x * w.z - v.z * w.x;
+            float zMult = v.x * w.y - v.y * w.x;
+
+            return (new Vector3(xMult, yMult, zMult));
+        }
+    
+        public static float Dot(Vector3 a, Vector3 b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+    }
+}
